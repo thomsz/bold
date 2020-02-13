4175e9676f02307d34abbf1c461efee7179fe897
Tom Segev <tom@golaender.com>
2020-02-13 14:04:22 +0100

    Add router

--- app/controllers/mainController.class.php
+++ app/controllers/mainController.class.php
@@ -3,6 +3,30 @@ namespace App\Controllers;
 
 class MainController {
 
+	/**
+	 * Router
+	 */
+	public static function router() {
 
+		$request = $_SERVER['REQUEST_URI'];
+
+		switch ($request) {
+			case '/user/new':
+				self::signup();
+				break;
+			
+			case '/user/signin':
+				self::signin();
+				break;
+
+			case '/user/verify_token':
+				self::verify_token();
+				break;
+
+			default:
+				break;
+		}
+
+	}
 
 }
