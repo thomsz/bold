18e9d60e39837335f432beda99fd695270e27abc
Tom Segev <tom@golaender.com>
2020-02-13 14:08:47 +0100

    Add autoloader

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php/** * Autoload classes */spl_autoload_extensions('.class.php');spl_autoload_register();?>Welcome home
+<?php/** * Autoload classes */spl_autoload_extensions('.classes.php');spl_autoload_register();header('Content-Type: application/json;charset=utf-8');/** * Router */App\Controllers\MainController::router();
