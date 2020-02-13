9f7872876fda04c0ed49d663baefe4376c428d42
Tom Segev <tom@golaender.com>
2020-02-13 15:43:54 +0100

    Fix wrong autoloader extension

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php/** * Autoload classes */spl_autoload_extensions('.classes.php');spl_autoload_register();header('Content-Type: application/json;charset=utf-8');/** * Router */App\Controllers\MainController::router();
+<?php
+/**
+ * Autoload classes
+ */
+spl_autoload_extensions('.class.php');
+spl_autoload_register();
+
+header('Content-Type: application/json;charset=utf-8');
+
+/**
+ * Router
+ */
+App\Controllers\MainController::router();
