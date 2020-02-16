8c35a23948b9726b51a9e16a59e64d66d916f991
Tom Segev <tom@golaender.com>
2020-02-16 14:52:51 +0100

    Add sign in form sends API request

--- front/src/views/signin.js
+++ front/src/views/signin.js
@@ -1,8 +1,93 @@
 import React from 'react';
 
 class Signin extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			email: '',
+			password: '',
+		};
+	}
+
+	_handleSubmit = event => {
+		event.preventDefault();
+		if (!this._validateForm()) return false;
+
+		this._signinUser();
+	};
+
+	_signinUser = () => {
+		const data = this.state;
+		console.log(data);
+
+		// Send API request
+		fetch('http://localhost:8888/api/user/signin', {
+			method: 'POST',
+			mode: 'no-cors',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		})
+			.then(response => {
+				return response.json();
+			})
+			.then(data => {
+				console.log('Success:', data);
+			})
+			.catch(error => {
+				console.error('Error:', error);
+			});
+	};
+
+	_handleInputChange = event => {
+		event.preventDefault();
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	};
+
+	_validateForm = () => {
+		return this._validateEmpty();
+	};
+
+	_validateEmpty = () => {
+		const stateValues = Object.values(this.state);
+		for (const value of stateValues) {
+			if (value === '') {
+				console.log('Please fill all fields');
+				return false;
+			}
+		}
+		return true;
+	};
+
 	render() {
-		return <div className="signin">Sign in</div>;
+		const { email } = this.state;
+		const { password } = this.state;
+
+		return (
+			<div className="signin">
+				<h1>Sign in</h1>
+				<form onSubmit={this._handleSubmit}>
+					<label htmlFor="email">email</label>
+					<input
+						name="email"
+						value={email}
+						onChange={this._handleInputChange}
+					/>
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						name="password"
+						value={password}
+						onChange={this._handleInputChange}
+					/>
+					<button type="submit">Sign in</button>
+				</form>
+			</div>
+		);
 	}
 }
 
